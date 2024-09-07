2c5e6d7670305789370f0e44718c8b338f9a67b7
zangyt <zangyt@yeah.net>
2024-09-07 20:32:03 +0800

    静态文件模板

--- heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/controller/v1/ArticleHomeController.java
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/controller/v1/ArticleHomeController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author zangyt
+ */
 @RestController
 @RequestMapping("/api/v1/article")
 public class ArticleHomeController {
--- heima-leadnews-test/minio-demo/src/test/java/com/heima/minio/test/MinIOTest.java
+++ heima-leadnews-test/minio-demo/src/test/java/com/heima/minio/test/MinIOTest.java
@@ -24,20 +24,20 @@ public class MinIOTest {
         FileInputStream fileInputStream = null;
         try {
             fileInputStream =
-                    new FileInputStream("/Users/zangyt/Downloads/明朝那些事儿 (当年明月) (Z-Library).epub");;
+                    new FileInputStream("/Users/zangyt/Documents/黑马头条/day02-app端文章查看，静态化freemarker,分布式文件系统minIO/资料/模板文件/plugins/js/axios.min.js");;
 
             //1.创建minio链接客户端
             MinioClient minioClient = MinioClient.builder().credentials("minio", "minio123").endpoint("http://47.94.142.78:9000").build();
             //2.上传
             PutObjectArgs putObjectArgs = PutObjectArgs.builder()
-                    .object("rary.epub")//文件名
-                    .contentType("epub")//文件类型
+                    .object("plugins/js/axios.min.js")//文件名
+                    .contentType("text/js")//文件类型
                     .bucket("leadnews")//桶名词  与minio创建的名词一致
                     .stream(fileInputStream, fileInputStream.available(), -1) //文件流
                     .build();
             minioClient.putObject(putObjectArgs);
 
-            System.out.println("http://192.168.200.130:9000/leadnews/ak47.jpg");
+//            System.out.println("http://192.168.200.130:9000/leadnews/ak47.jpg");
 
         } catch (Exception ex) {
             ex.printStackTrace();
